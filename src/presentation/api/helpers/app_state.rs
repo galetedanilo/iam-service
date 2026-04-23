b1a4a3d842c5f7ede1a545c04cbf1e1675bc5990
Danilo Galete <galetedanilo@gmail.com>
2026-04-22 22:18:58 -0300

    refector: breaking down responsibilites

--- src/presentation/api/helpers/app_state.rs
+++ src/presentation/api/helpers/app_state.rs
@@ -3,12 +3,15 @@ use std::sync::Arc;
 use jsonwebtoken::EncodingKey;
 
 use crate::{
-    application::use_cases::register_user_use_case::RegisterUserUseCase,
+    application::use_cases::{
+        confirm_email_use_case::ConfirmEmailUseCase, register_user_use_case::RegisterUserUseCase,
+    },
     domain::repositories::user_repository::UserRepository,
 };
 
 #[derive(Clone)]
 pub struct AppState<R: UserRepository> {
+    pub confirm_email_use_case: Arc<ConfirmEmailUseCase<R>>,
     pub register_use_user_case: Arc<RegisterUserUseCase<R>>,
     pub encoding_key: Arc<EncodingKey>,
 }
@@ -16,7 +19,8 @@ pub struct AppState<R: UserRepository> {
 impl<R: UserRepository> AppState<R> {
     pub fn new(repository: Arc<R>, encoding_key: Arc<EncodingKey>) -> Self {
         Self {
-            register_use_user_case: Arc::new(RegisterUserUseCase::new(repository)),
+            confirm_email_use_case: Arc::new(ConfirmEmailUseCase::new(repository.clone())),
+            register_use_user_case: Arc::new(RegisterUserUseCase::new(repository.clone())),
             encoding_key,
         }
     }
--- src/presentation/api/helpers/validated_json.rs
+++ src/presentation/api/helpers/validated_json.rs
@@ -1,66 +1,10 @@
-use axum::{
-    Json,
-    body::Body,
-    extract::FromRequest,
-    http::{Request, StatusCode},
-    response::IntoResponse,
-};
-use chrono::{DateTime, Utc};
+use axum::{Json, body::Body, extract::FromRequest, http::Request};
+use chrono::Utc;
 
-use serde::Serialize;
 use serde::de::DeserializeOwned;
 use validator::Validate;
 
-use crate::domain::models::user::UserError;
-
-#[derive(Debug, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct AppErrorResponse {
-    pub message: String,
-    pub status_code: u16,
-    pub timestamp: DateTime<Utc>,
-    pub code: Option<String>,
-    pub details: Option<String>,
-}
-
-impl IntoResponse for AppErrorResponse {
-    fn into_response(self) -> axum::response::Response {
-        let status_code =
-            StatusCode::from_u16(self.status_code).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
-
-        (status_code, axum::Json(self)).into_response()
-    }
-}
-
-impl From<UserError> for AppErrorResponse {
-    fn from(error: UserError) -> Self {
-        let (message, status_code, details) = match error {
-            UserError::AlreadyExists(details) => (
-                "User already exists".to_string(),
-                409,
-                Some(details.to_string()),
-            ),
-
-            UserError::InvalidData(details) => {
-                ("Invalid data".to_string(), 400, Some(details.to_string()))
-            }
-            UserError::NotFound(details) => {
-                ("User not found".to_string(), 404, Some(details.to_string()))
-            }
-            UserError::Unknown(details) => {
-                ("Unknown error".to_string(), 500, Some(details.to_string()))
-            }
-        };
-
-        Self {
-            message,
-            status_code,
-            timestamp: Utc::now(),
-            code: None,
-            details,
-        }
-    }
-}
+use crate::presentation::api::helpers::app_error::AppErrorResponse;
 
 pub struct ValidatedJson<T>(pub T);
 
@@ -120,7 +64,12 @@ mod tests {
     use crate::presentation::api::requests::register_user_request::RegisterUserRequest;
 
     use super::*;
-    use axum::{Router, body::Body, http::Request, routing::post};
+    use axum::{
+        Router,
+        body::Body,
+        http::{Request, StatusCode},
+        routing::post,
+    };
     use serde_json::json;
     use tower::ServiceExt;
 
