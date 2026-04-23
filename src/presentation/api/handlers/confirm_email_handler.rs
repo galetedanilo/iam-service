f543c6069aba0dcf11ed8c9a4785865ba5cbd9bd
Danilo Galete <galetedanilo@gmail.com>
2026-04-22 22:21:59 -0300

    feat: confirm email

---
+++ src/presentation/api/handlers/confirm_email_handler.rs
@@ -0,0 +1,24 @@
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+};
+
+use crate::{
+    application::inputs::confirm_email_input::ConfirmEmailInput,
+    domain::repositories::user_repository::UserRepository,
+    presentation::api::helpers::{app_error::AppErrorResponse, app_state::AppState},
+};
+
+pub async fn confirm_email_handler<R: UserRepository>(
+    State(state): State<AppState<R>>,
+    Path((user_id, token)): Path<(String, String)>,
+) -> Result<StatusCode, AppErrorResponse> {
+    let command = ConfirmEmailInput::try_new(user_id, token)?;
+
+    state
+        .confirm_email_use_case
+        .execute(command)
+        .await
+        .map_err(AppErrorResponse::from)
+        .map(|_| StatusCode::OK)
+}
